839969a088f9ddb1134b246bf3708f8a8fde20f9
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-20 10:49:10 -0600

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+include(":FaridExtensions")
