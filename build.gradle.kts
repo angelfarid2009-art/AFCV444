7ad81e31877481e7142149b80bd6d6bdb5102c03
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 00:52:10 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,14 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
+        maven("https://jitpack.io")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.github.recloudstream:gradle:81b1d424d2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
-
-plugins {
-    id("com.lagradost.cloudstream3.gradle") version "0.6.2" apply false
-}
-
 tasks.register("makePlugins") {
     dependsOn(subprojects.map { ":${it.name}:make" })
 }
