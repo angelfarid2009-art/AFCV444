a3b84b02a9835a6caa64c73d3bc26537b3045842
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 00:51:39 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "AFCV444"
 include(":FaridExtensions")
 project(":FaridExtensions").projectDir = File("AFCV444/FaridExtensions")
