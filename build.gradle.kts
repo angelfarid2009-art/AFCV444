d6e8ae7bf14c02961e7a87400e7070f4d8623362
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 00:08:24 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    }
+}
+
 plugins {
-    id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.lagradost.cloudstream3.gradle") version "0.6.2" apply false
+}
+
+tasks.register("makePlugins") {
+    dependsOn(subprojects.map { ":${it.name}:make" })
 }
