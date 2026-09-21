9135adefb4044a2fd344b27f7081b7984bcceb22
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 00:46:26 -0600

    Update build.gradle.kts

--- FaridExtensions/build.gradle.kts
+++ FaridExtensions/build.gradle.kts
@@ -1,26 +1,13 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle") version "0.6.2"
 }
+
+cloudstream {
+    language = "es"
+    authors = listOf("Farid")
+    description = "Plugin de Farid"
+}
+
 android {
-    compileSdk = 34
-    namespace = "com.farid.otakustv"
-    defaultConfig { minSdk = 21 }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions { jvmTarget = "1.8" }
-}
-dependencies {
-    compileOnly("com.github.recloudstream:cloudstream:master-SNAPSHOT")
-}
-tasks.register("make") {
-    dependsOn("assembleDebug")
-    doLast {
-        val jarFile = file("build/intermediates/aar_main_jar/debug/classes.jar")
-        val outFile = file("build/OtakusTV.cs3")
-        outFile.parentFile.mkdirs()
-        if (jarFile.exists()) jarFile.copyTo(outFile, overwrite = true)
-    }
+    namespace = "com.farid.afcv444"
 }
