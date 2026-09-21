d2d44b3d4e6a79d0b944bbe88df54d29607b6a56
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 00:57:58 -0600

    Update build.gradle.kts

--- FaridExtensions/build.gradle.kts
+++ FaridExtensions/build.gradle.kts
@@ -1,10 +1,7 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("com.lagradost.cloudstream3.gradle")
-}
 cloudstream {
     authors = listOf("Farid")
+    description = "Extension de Farid"
+    language = "es"
 }
 android {
     namespace = "com.farid"
