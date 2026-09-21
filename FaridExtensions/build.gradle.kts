2dd767a49ae0b88282e04e9d9e1c59466d079291
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 01:09:57 -0600

    Update build.gradle.kts

--- FaridExtensions/build.gradle.kts
+++ FaridExtensions/build.gradle.kts
@@ -1,10 +1,8 @@
 cloudstream {
     authors = listOf("Farid")
 }
+
 android {
-    compileSdkVersion(34)
+    compileSdk = 34
     namespace = "com.farid"
-    defaultConfig {
-        minSdk = 21
-    }
 }
