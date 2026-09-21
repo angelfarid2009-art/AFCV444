2aa6aeb4291b6c1cfe46463018b55c0c1d68f9a1
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 01:27:07 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,13 +16,13 @@ subprojects {
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
     
-    // ESTO ES LO QUE TE BLINDA
-    extensions.findByName("android")?.let {
-        (it as com.android.build.gradle.LibraryExtension).apply {
-            compileSdk = 34
-            defaultConfig {
-                minSdk = 21
-            }
+    // ESTO ES LO QUE YA TE VA A FORZAR EL NAMESPACE Y EL SDK
+    val android = extensions.findByName("android") as? com.android.build.gradle.LibraryExtension
+    android?.apply {
+        compileSdk = 34
+        namespace = "com.farid"
+        defaultConfig {
+            minSdk = 21
         }
     }
 
