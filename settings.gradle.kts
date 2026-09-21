70b767568b43546f589a3934208efe7d640daec9
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 00:27:24 -0600

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "AFCV444"
-
-file(rootDir).listFiles()?.forEach { dir ->
-    if (dir.isDirectory && File(dir, "build.gradle.kts").exists()) {
-        if (dir.name != "buildSrc") {
-            include(":${dir.name}")
-        }
-    }
-}
