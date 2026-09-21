f4c0d330a1b4327c36c106fbc795430559dd0b98
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-21 01:01:53 -0600

    Update build.gradle.kts

--- FaridExtensions/build.gradle.kts
+++ FaridExtensions/build.gradle.kts
@@ -4,5 +4,15 @@ cloudstream {
     language = "es"
 }
 android {
+    compileSdkVersion(34)
     namespace = "com.farid"
+    
+    defaultConfig {
+        minSdk = 21
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
