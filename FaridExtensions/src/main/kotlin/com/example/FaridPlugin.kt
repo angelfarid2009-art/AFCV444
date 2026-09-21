af30f50fd57df551342f3f306fedb8c2c34af3e4
angelfarid2009-art <angelfarid2009@gmail.com>
2026-09-20 23:10:21 -0600

    Actualizar FaridPlugin.kt

--- FaridExtensions/src/main/kotlin/com/example/FaridPlugin.kt
+++ FaridExtensions/src/main/kotlin/com/example/FaridPlugin.kt
@@ -7,5 +7,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class FaridPlugin: Plugin() {
     override fun load() {
         registerMainAPI(OtakusTVProvider())
+        registerMainAPI(HenaoJaraProvider())
     }
 }
